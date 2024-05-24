6f2b1c2861571c095d70fcb4d1eea2002b0748a0
Maximiliano Gordillo <724910@student.dusd.net>
2024-05-23 23:50:14 -0700

    dice roll determines movement
    you can roll the dice and it now determines how many spaces you move. i think it works pretty well :)
    
    ~Max signing out...

---
+++ GameDesignFinalDublanGordillo/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject characterFocus;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = characterFocus.transform.position + new Vector3(0, 4, -7);
+    }
+}
--- GameDesignFinalDublanGordillo/Assets/Scripts/Dice.cs
+++ GameDesignFinalDublanGordillo/Assets/Scripts/Dice.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Dice : MonoBehaviour
 {
+    public GameObject character;
     bool rolling = true;
+    int roll = 1;
+    public List<Vector3> rollValues = new List<Vector3>();
     
     void Start()
     {
@@ -16,12 +19,23 @@ public class Dice : MonoBehaviour
     {
         if (rolling)
         {
-            transform.Rotate(new Vector3(-666, 420, 69) * Time.deltaTime);
+            transform.Rotate(new Vector3(-6666, 4200, 690) * Time.deltaTime);
         }
+        transform.position = character.transform.position+new Vector3(0, 2, 0);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rolling = false;
+            if (rolling)
+            {
+                rolling = false;
+                roll = Random.Range(1, 6);
+                transform.eulerAngles = rollValues[roll-1];
+                character.GetComponent<SpaceTraversal>().spacesLeft = roll-1;
+            }
+            else
+            {
+                rolling = true;
+            }
         }
     }
 }
